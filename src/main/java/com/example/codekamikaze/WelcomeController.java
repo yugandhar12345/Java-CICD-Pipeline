69a8bfed0c767c7d55b3187c187aea20685f6a92
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2025-04-26 15:51:25 +0530

    Update WelcomeController.java

--- src/main/java/com/example/codekamikaze/WelcomeController.java
+++ src/main/java/com/example/codekamikaze/WelcomeController.java
@@ -1,16 +1,7 @@
 package com.example.codekamikaze;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-public class CodeKamikazeApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CodeKamikazeApplication.class, args);
-    }
-}
 
 @RestController
 class WelcomeController {
