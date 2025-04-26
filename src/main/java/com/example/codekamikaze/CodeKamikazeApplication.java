4bbdb7af9387e6bc8db5635539ee2f6be8f4e3cf
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2025-04-26 08:27:21 +0530

    Update CodeKamikazeApplication.java

--- src/main/java/com/example/codekamikaze/CodeKamikazeApplication.java
+++ src/main/java/com/example/codekamikaze/CodeKamikazeApplication.java
@@ -1,17 +1,11 @@
 package com.example.codekamikaze;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CodeKamikazeApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(CodeKamikazeApplication.class, args);
-        System.out.println("Welcome to CodeKamikaze");
+    public static String getWelcomeMessage() {
+        return "Welcome to CodeKamikaze";
     }
 
-    public String getWelcomeMessage() {
-        return "Welcome to CodeKamikaze";
+    public static void main(String[] args) {
+        System.out.println(getWelcomeMessage());
     }
 }
