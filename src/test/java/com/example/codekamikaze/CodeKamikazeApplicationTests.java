b5302e884770c8b36f4e87db5cf9f3ab79efa1ce
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2025-04-26 08:28:16 +0530

    Update CodeKamikazeApplicationTests.java

--- src/test/java/com/example/codekamikaze/CodeKamikazeApplicationTests.java
+++ src/test/java/com/example/codekamikaze/CodeKamikazeApplicationTests.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CodeKamikazeApplicationTests {
-
     @Test
     void testWelcomeMessage() {
-        CodeKamikazeApplication app = new CodeKamikazeApplication();
-        assertEquals("Welcome to CodeKamikaze", app.getWelcomeMessage());
+        assertEquals("Welcome to CodeKamikaze", CodeKamikazeApplication.getWelcomeMessage());
     }
 }
