1e2a59f099ffca1a5d3d7cfb20c7000721bb029c
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2025-04-26 15:56:22 +0530

    Update WelcomeController.java

--- src/main/java/com/example/codekamikaze/WelcomeController.java
+++ src/main/java/com/example/codekamikaze/WelcomeController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class WelcomeController {
+public class WelcomeController {
+
     @GetMapping("/welcome")
     public String getWelcomeMessage() {
         return "Welcome to CodeKamikaze";
